5ce5bdaad01b726f91f80acf23b1f8c67177e4c9
Martin Vasapollo <martin.vsa@gmail.com>
2020-10-29 23:27:18 -0300

    Actualizado el readme con instrucciones y comentado el codigo

--- readme.md
+++ readme.md
@@ -1,4 +1,29 @@
 # Joy To Vesc
+
 ## Facultad de Ingenieria del Ejercito
 
 Paquete ROS que mapea el input de un nodo Joy_ROS a uno o mas nodos Vesc-ROS-FW-3.33, ambos requeridos. La finalidad es poder controlar duty_cycle y break del driver VESC desde un Joystick tipo PS3 para realizar pruebas de funcionamiento.
+
+### Requisitos
+
+* ROS Melodic
+* ros-drivers/joystick_drivers
+* raess1/Vesc-ROS-FW-3.33
+
+### Instalacion
+
+```bash
+cd ~/catkin_ws/src
+git clone https://github.com/carlosmgc2003/joy_to_vesc.git
+cd ..
+catkin_make -DCATKIN_WHITELIST_PACKAGES="joy_to_vesc"
+
+```
+
+### Utilizacion
+
+```bash
+rosrun joy_to_vesc joy_to_vesc_node _ratio:=0.5
+```
+
+Ratio el un multiplicador del input del joystick [-1.0;1.0]. El parametro ratio es opcional. Su valor por defecto es 0.3.
--- src/joy_to_vesc_node.cpp
+++ src/joy_to_vesc_node.cpp
@@ -5,34 +5,69 @@
 
 // Constante global que define la posicion en el array Float64 del eje Y del Stick Analogico Izquierdo del Joystick PS3
 const int LINEAR_AXIS = 1;
+const int RED_BUTTON = 1;
+const double DEFAULT_RATIO = 0.3;
 
+
+// Clase que convierte el input del Joystick en instrucciones para Drivers VESC
 class Translator {
     ros::NodeHandle n;
+    ros::NodeHandle private_n;
     ros::Subscriber joystick;
-    ros::Publisher vescDriver;
-    float ratio;
+    ros::Publisher vescMotor;
+    ros::Publisher vescBrake;
+    double ratio;
 public:
     Translator();
     void joystickCallback(const sensor_msgs::Joy::ConstPtr&);
+    void setRatio();
 };
 
 Translator::Translator(){
+    // Manejador del Nodo
     n = ros::NodeHandle();
-    if(n.param<float>("ratio", ratio, 0.3)){
-        ROS_INFO("Coeficiente de Duty Cycle: [%.2f]", ratio);
-    } else {
-        ROS_INFO("Coeficiente de Duty Cycle por defecto: [%.2f]", ratio);
-    }
+    // Instancia privada solamente para aceder a los parametros
+    private_n = ros::NodeHandle("~");
+    setRatio();
+    // Captura del Parametro ratio, que define el multiplicador de reduccion del motor
+    
     joystick = n.subscribe<sensor_msgs::Joy>("joy", 10, &Translator::joystickCallback, this);
-    vescDriver =  n.advertise<std_msgs::Float64>("commands/motor/duty_cycle", 10);
+    vescMotor =  n.advertise<std_msgs::Float64>("commands/motor/duty_cycle", 10);
+    vescBrake =  n.advertise<std_msgs::Float64>("commands/motor/brake", 10);
+}
+
+void Translator::setRatio() {
+    if(private_n.param<double>("ratio", ratio, DEFAULT_RATIO)){
+        if(ratio >= 1.0 || ratio <= 0.1) {
+            ratio = DEFAULT_RATIO;
+            ROS_INFO("El ratio seteado es invalido.");
+        }
+        ROS_INFO("Ratio seteado [%.2f]", ratio);
+    } else {
+        ROS_INFO("Ratio por defecto [%.2f]", ratio);
+    }
 }
 
 void Translator::joystickCallback(const sensor_msgs::Joy::ConstPtr& joy) {
-    float y_value = joy->axes[LINEAR_AXIS];
-    ROS_INFO("El pad devuelve: [%.2f]", y_value);
-    std_msgs::Float64 msg;
-    msg.data = y_value * ratio;
-    vescDriver.publish(msg);
+    // Captura de los valores de los botones de Joystick
+    const double y_value = joy->axes[LINEAR_AXIS];
+    const int red_value = joy->buttons[RED_BUTTON];
+    // Instancia de los mensajes
+    std_msgs::Float64 msgMotor;
+    std_msgs::Float64 msgBrake;
+
+    // Carga de los datos
+    msgMotor.data = y_value * ratio;
+    msgBrake.data = red_value;
+
+    // Feedback en el log
+    ROS_INFO("El pad devuelve: [%.2f], VESC recibe [%.2f], Brake en [%.2f]", y_value, msgMotor.data, msgBrake.data);
+    
+    // Publicacion de los datos
+    vescMotor.publish(msgMotor);
+    // Solo se publica freno cuando cambia
+    if(red_value != 0)
+        vescBrake.publish(msgBrake);
 }
 
 int main(int argc, char **argv) {
